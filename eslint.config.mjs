de402bc6c8ce777ac69d8de9a751b05a4f7ccdf8
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-10-24 00:58:36 +0200

    Maintenance update - Update devDependencies - Replace eslint-plugin-import-x by eslint-plugin-import - Don't prettify *.mjs - Change some ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,29 @@
-import eslintPluginImportX from "eslint-plugin-import-x";
+import eslintPluginImport from "eslint-plugin-import";
 import eslintPluginJs from "@eslint/js";
 import eslintPluginStylistic from "@stylistic/eslint-plugin";
 import globals from "globals";
 
 const config = [
+  eslintPluginImport.flatConfigs.recommended,
   eslintPluginJs.configs.all,
-  eslintPluginImportX.flatConfigs.recommended,
   {
-    files: ["**/*.js"],
-    languageOptions: {
-      globals: {
+    "files": ["**/*.js"],
+    "languageOptions": {
+      "globals": {
         ...globals.browser,
         ...globals.node,
-        Log: "readonly",
-        Module: "readonly",
-        WeatherObject: "readonly",
-        config: "readonly",
-        moment: "readonly"
-      }
+        "Log": "readonly",
+        "Module": "readonly",
+        "WeatherObject": "readonly",
+        "config": "readonly",
+        "moment": "readonly"
+      },
+      "sourceType": "commonjs"
     },
-    plugins: {
+    "plugins": {
       ...eslintPluginStylistic.configs["all-flat"].plugins
     },
-    rules: {
+    "rules": {
       ...eslintPluginStylistic.configs["all-flat"].rules,
       "@stylistic/array-element-newline": ["error", "consistent"],
       "@stylistic/dot-location": ["error", "property"],
@@ -32,7 +33,7 @@ const config = [
       "@stylistic/padded-blocks": ["error", "never"],
       "@stylistic/quote-props": ["error", "as-needed"],
       "capitalized-comments": "off",
-      complexity: "off",
+      "complexity": "off",
       "consistent-this": "off",
       "id-length": "off",
       "init-declarations": "off",
@@ -47,34 +48,30 @@ const config = [
       "no-negated-condition": "off",
       "no-ternary": "off",
       "one-var": "off",
-      "sort-keys": "off"
+      "sort-keys": "off",
+      "strict": "off"
     }
   },
   {
-    files: ["**/*.mjs"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      globals: {
+    "files": ["**/*.mjs"],
+    "languageOptions": {
+      "ecmaVersion": "latest",
+      "globals": {
         ...globals.node
       },
-      sourceType: "module"
+      "sourceType": "module"
     },
-    plugins: {
+    "plugins": {
       ...eslintPluginStylistic.configs["all-flat"].plugins
     },
-    rules: {
+    "rules": {
       ...eslintPluginStylistic.configs["all-flat"].rules,
       "@stylistic/array-element-newline": "off",
-      "@stylistic/dot-location": ["error", "property"],
-      "@stylistic/function-call-argument-newline": ["error", "consistent"],
-      "@stylistic/function-paren-newline": "off",
       "@stylistic/indent": ["error", 2],
       "@stylistic/padded-blocks": ["error", "never"],
-      "@stylistic/quote-props": ["error", "as-needed"],
       "func-style": "off",
       "max-lines-per-function": ["error", 100],
       "no-magic-numbers": "off",
-      "no-undefined": "off",
       "one-var": "off",
       "prefer-destructuring": "off"
     }
